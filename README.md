af5f72cb69bae4d4b02fe62897afd200919449fe
hux <huxiao1@idea.edu.cn>
2025-02-26 11:34:02 +0800

    update readme and code

--- README.md
+++ README.md
@@ -4,45 +4,62 @@
 3. ~~add python version;~~
 
 # CCenterFinder
-This repository contains our implementation on accurately localizing 3D and 2D circular centers, including:
 
-1. A 3D circular center finding algorithm based on Conformal Algebra. We also provide a RANSAC variant so that it can handle  outliers.
-2. An exact 2D circular center finding algorithm that uses grid search. To solve the two-fold ambiguity problem, we give a feasible solution given two coplanar circles.
-3. We show in some automation and robotics application user cases that the proposed method can help users improve accuracy.
+This repository contains our implementation for accurately localizing 3D and 2D circular centers, including:
 
-# Dependencies
-For the C++ version:
+1. A 3D circular center finding algorithm based on Conformal Algebra. We also provide a RANSAC variant to handle outliers.
+2. An exact 2D circular center finding algorithm that uses grid search. To solve the two-fold ambiguity problem, we provide a feasible solution given two coplanar circles.
+3. Demonstrations in automation and robotics applications showing how the proposed method can help improve accuracy.
+
+## Dependencies
+
+### For the C++ version:
 1. Eigen3
 2. OpenCV (REQUIRED for examples)
 3. PCL (REQUIRED for examples)
 
-For the Python version:
+### For the Python version:
 1. NumPy
 2. OpenCV (optional)
 3. Matplotlib (optional)
 
-For RANSAC：
-1. we borrow template code from: https://github.com/mint-lab/rtl
+### For RANSAC:
+We borrow template code from: [GRANSAC](https://github.com/drsrinathsridhar/GRANSAC/tree/master) and [Ransaclib](https://github.com/tsattler/RansacLib/tree/master)
 
-# Data
-To generate Monte-Carlo experimental data, change codes like `os.makedirs('/mnt/d/data/IROS/data/3d_experiment',exist_ok=True)` according to your folder structure, run
+## Data
+
+To generate Monte-Carlo experimental data, modify the code to match your folder structure, e.g., `os.makedirs('/mnt/d/data/IROS/data/3d_experiment', exist_ok=True)`, and run:
 ```bash
-python main_carlo_pcl_3d.py 
+python main_carlo_pcl_3d.py --output_folder /path/to/output_folder
 ```
-todo: add argparser to specify output folder for generated data.
 
+## Usage
 
-# Usage
+### Running the C++ version
+Compile and run the benchmark:
+```bash
+g++ -o benchmark_circle_fitting_noise src/benchmark_circle_fitting_noise.cpp -I/path/to/eigen -I/path/to/opencv -I/path/to/pcl -L/path/to/libs -lopencv_core -lopencv_imgproc -lopencv_highgui -lpcl_common -lpcl_io -lpcl_segmentation
+./benchmark_circle_fitting_noise [benchmark_choice]
+```
+`benchmark_choice` can be `0` for noise benchmark or `1` for span benchmark.
 
-# Citation
+### Running the Python version
+Generate data and run experiments:
+```bash
+python python/main_carlo_pcl_3d.py --output_folder /path/to/output_folder
+```
 
-# Acknowlegement 
+## Citation
 
-[GRANSAC](https://github.com/drsrinathsridhar/GRANSAC/tree/master)
+If you use this code, please cite our work.
 
-[Ransaclib](https://github.com/tsattler/RansacLib/tree/master)
+## Acknowledgement
 
-# Contributors
+We acknowledge the use of the following libraries:
+- [GRANSAC](https://github.com/drsrinathsridhar/GRANSAC/tree/master)
+- [Ransaclib](https://github.com/tsattler/RansacLib/tree/master)
+
+## Contributors
 1. levinson (xiahaa)
 2. jun (jjj)
 3. lwc (leovinchen)
--- python/main_carlo_pcl_3d.py
+++ python/main_carlo_pcl_3d.py
@@ -2,16 +2,20 @@ import numpy as np
 import cv2 as cv
 import os
 import sys
+import argparse
 import matplotlib.pyplot as plt
 # import mql_toolkits.mplot3d.axes3d as axes3d
 # %matplotlib qt
 
 from experiment_3d_pcl import  *
-
 from tqdm import tqdm, trange
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Monte-Carlo experimental data generation")
+    parser.add_argument('--output_folder', type=str, required=True, help='Output folder for generated data')
+    return parser.parse_args()
 
-def simu_1():
+def simu_1(output_folder):
     num_experiments=1000
     center_error = np.zeros((num_experiments,2))
 
@@ -31,38 +35,29 @@ def simu_1():
     print(np.mean(center_error[:,0]))
     print(np.mean(center_error[:,1]))
 
-
-def simu_2():
-    # simu 2
+def simu_2(output_folder):
     num_experiments=500
     noise_levels = np.array([1e-4,1e-3,1e-2,1e-1,1])
 
-    # use tqdm
     pbar=tqdm(total=num_experiments*len(noise_levels))
     for noise_level in noise_levels:
         i=0
         while i < num_experiments:
             centers,pcn=monte_carlo_experiment(noise_level=noise_level,threshold=0.01,do_refinement=True,data_gen=True)
 
-            # write out pcn to a file, the output folder is /mnt/d/data/IROS/data/3d_experiment
-            import os
-            os.makedirs('/mnt/d/data/IROS/data/3d_experiment',exist_ok=True)
-            # write point line by line
-            # output file name is composed of noise level and experiment id
+            os.makedirs(output_folder, exist_ok=True)
+            filename = os.path.join(output_folder, 'pcn_{:.6f}_{:04d}.txt'.format(noise_level, i))
 
-            filename = '/mnt/d/data/IROS/data/3d_experiment/pcn_{:.6f}_{:04d}.txt'.format(noise_level,i)
-
-            with open(filename,'w') as f:
+            with open(filename, 'w') as f:
                 for j in range(pcn.shape[1]):
                     f.write(f'{pcn[0,j]} {pcn[1,j]} {pcn[2,j]}\n')
 
-            # write out also the result to a file, line by line
-            filename = '/mnt/d/data/IROS/data/3d_experiment/centers_{:.6f}_{:04d}.txt'.format(noise_level,i)
-            np.savetxt(filename,np.array([centers['real'],centers['pcl'],centers['lsq']]))
+            filename = os.path.join(output_folder, 'centers_{:.6f}_{:04d}.txt'.format(noise_level, i))
+            np.savetxt(filename, np.array([centers['real'], centers['pcl'], centers['lsq']]))
             pbar.update(1)
             i+=1
 
-def simu_3():
+def simu_3(output_folder):
 
     def gen_data_span(span_angle, noise_level):
         r=2+np.random.rand(1)*3
@@ -82,32 +77,26 @@ def simu_3():
         centers['real']=t.squeeze()
         return (centers,pcn)
 
-    # simu 2
     num_experiments=500
     noise = 1e-1
     span_angle = np.array([90, 135, 180, 225, 270, 315, 360])
-    # use tqdm
     pbar=tqdm(total=num_experiments*len(span_angle))
     for span in span_angle:
         i=0
         while i < num_experiments:
             centers,pcn=gen_data_span(span, noise)
-            # write out pcn to a file, the output folder is /mnt/d/data/IROS/data/3d_experiment
-            import os
-            base_dir = '/mnt/d/data/IROS/data/3d_experiment_span'
-            os.makedirs(base_dir,exist_ok=True)
-            # write point line by line
-            # output file name is composed of noise level and experiment id
+            base_dir = os.path.join(output_folder, '3d_experiment_span')
+            os.makedirs(base_dir, exist_ok=True)
             filename = os.path.join(base_dir, 'pcn_{:03d}_{:.6f}_{:04d}.txt'.format(span, noise, i))
             with open(filename,'w') as f:
                 for j in range(pcn.shape[1]):
                     f.write(f'{pcn[0,j]} {pcn[1,j]} {pcn[2,j]}\n')
 
-            # write out also the result to a file, line by line
             filename = os.path.join(base_dir, 'centers_{:03d}_{:.6f}_{:04d}.txt'.format(span, noise, i))
             np.savetxt(filename,np.array([centers['real'],centers['pcl'],centers['lsq']]))
             pbar.update(1)
             i+=1
 
 if __name__=='__main__':
-    simu_3()
+    args = parse_args()
+    simu_3(args.output_folder)
